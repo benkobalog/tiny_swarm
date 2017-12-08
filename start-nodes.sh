1c187a6a570041c0ee6202cc70c64d479710a319
benkobalog <benko.balog@gmail.com>
2017-12-08 16:31:54 +0100

    updated docker version

--- start-nodes.sh
+++ start-nodes.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
+
+# == helper functions ==
+get_state() {
+    read header
+    read firstLine
+    state=$(echo $firstLine | cut -f $1 -d ' ')
+    echo $state
+}
+
+is_ready() {
+    while [ $($1 | get_state $2) != $3 ]; do  echo "Waiting"; done 
+}
+
+wait_for_service_up() {
+    is_ready "$1" 5 "Running"
+}
+
+wait_for_nodes_up() {
+    lsOut=$(docker node ls)
+
+    nrAll=$(echo "$lsOut" | wc -l)
+    nrActive=$(echo "$lsOut" | grep Active | wc -l)
+
+    echo $nrAll
+    echo $nrActive
+    if [ $((nrAll - 1)) != $nrActive ]; then
+	echo "Waiting for nodes to be active"
+	waitfor
+    fi
+}
+
+# ======================
 #set -x
 localIP=$(ifconfig enp0s3 | grep -oP "inet addr:[0-9.]{7,15} " | cut -f 2 -d :)
 
 # Start manager node
-startNodeCMD=$(docker swarm init --advertise-addr $localIP | grep 'docker swarm join \\' -A2 | sed  's/\\//g' | tr '\n' ' ')
+startNodeCMD=$(docker swarm init --advertise-addr $localIP | grep 'docker swarm join')
 
 nodes="wnode@worker1 wnode@worker2"
 
@@ -18,6 +50,9 @@ do
     ssh $node "$startNodeCMD"
 done
 
+wait_for_nodes_up
+
+
 # Start services
 networkName="flink_network"
 dockerCreate="docker service create --network $networkName"
@@ -25,5 +60,26 @@ dockerCreate="docker service create --network $networkName"
 docker network create --driver overlay $networkName
 
 $dockerCreate --name zookeeper1 --publish 2181:2181 kaeblo/zookeeper
+
+wait_for_service_up "docker service ps zookeeper1"
+
 $dockerCreate --name kafka1 --publish 9092:9092  --replicas 3   kaeblo/kafka
-$dockerCreate --name flink1     kaeblo/flink1
+
+wait_for_service_up "docker service ps kafka1"
+
+# Map Flink's jobmanager's port to a different port, schema registry uses this port as well 
+docker service create \
+       -e JOB_MANAGER_RPC_ADDRESS=flink-jobmanager \
+       -p 8089:8081 \
+       --network $networkName \
+       --name flink-jobmanager \
+       flink jobmanager
+
+wait_for_service_up "docker service ps flink-jobmanager"
+
+docker service create \
+       -e JOB_MANAGER_RPC_ADDRESS=flink-jobmanager \
+       --network $networkName \
+       --name flink-taskmanager \
+       --replicas 3 \
+       flink taskmanager 
